a35fc73e5c1e426e857f950385e60d0e6348ca35
marioiordanov <mio@shardlabs.io>
2024-09-02 12:39:44 +0300

    simulate array

--- test/SimulateArray.t.sol
+++ test/SimulateArray.t.sol
@@ -22,14 +22,68 @@ contract SimulateArrayTest is Test, NonMatchingSelectorHelper {
     SimulateArray public simulateArray;
 
     function setUp() public {
-        simulateArray = SimulateArray(HuffDeployer.config().deploy("SimulateArray"));
+        simulateArray = SimulateArray(
+            HuffDeployer.config().deploy("SimulateArray")
+        );
+    }
+
+    function testSimpleSimulateArray() external {
+        vm.expectRevert(bytes4(keccak256("ZeroArray()")));
+        simulateArray.popp();
+        (bool success, bytes memory returnData) = address(simulateArray).call(
+            abi.encodeWithSelector(SimulateArray.read.selector, 0)
+        );
+        assert(!success);
+        assertEq(bytes4(returnData), bytes4(keccak256("OutOfBounds()")));
+        console.log(simulateArray.length());
+        simulateArray.pushh(1);
+        console.log(simulateArray.length());
+        simulateArray.pushh(2);
+        console.log(simulateArray.length());
+        simulateArray.popp();
+        console.log(simulateArray.length());
+        simulateArray.popp();
+        console.log(simulateArray.length());
+        (success, returnData) = address(simulateArray).call(
+            abi.encodeWithSelector(SimulateArray.popp.selector)
+        );
+        assert(!success);
+        assertEq(
+            bytes4(returnData),
+            bytes4(keccak256("ZeroArray()")),
+            "Expected the call to revert with custom error 'ZeroArray()' but it didn't "
+        );
+
+        simulateArray.pushh(11);
+        simulateArray.pushh(21);
+        simulateArray.pushh(13);
+        assertEq(simulateArray.read(2), 13);
+        simulateArray.popp();
+        (success, returnData) = address(simulateArray).call(
+            abi.encodeWithSelector(SimulateArray.read.selector, 2)
+        );
+        assert(!success);
+        assertEq(
+            bytes4(returnData),
+            bytes4(keccak256("OutOfBounds()")),
+            "Expected the call to revert with custom error 'OutOfBounds()' but it didn't "
+        );
+
+        simulateArray.write(0, 100);
+        assertEq(simulateArray.read(0), 100);
+        vm.expectRevert();
+        simulateArray.write(100, 0);
     }
 
     function testSimulateArray(uint256[] memory array) external {
         vm.expectRevert(bytes4(keccak256("ZeroArray()")));
         simulateArray.popp();
 
-        assertEq(simulateArray.length(), 0, "length is initially meant to be 0");
+        assertEq(
+            simulateArray.length(),
+            0,
+            "length is initially meant to be 0"
+        );
 
         for (uint256 i; i < array.length; ++i) {
             simulateArray.pushh(array[i]);
@@ -62,7 +116,11 @@ contract SimulateArrayTest is Test, NonMatchingSelectorHelper {
         func_selectors[3] = SimulateArray.length.selector;
         func_selectors[4] = SimulateArray.write.selector;
 
-        bool success = nonMatchingSelectorHelper(func_selectors, callData, address(simulateArray));
+        bool success = nonMatchingSelectorHelper(
+            func_selectors,
+            callData,
+            address(simulateArray)
+        );
         assert(!success);
     }
 }
